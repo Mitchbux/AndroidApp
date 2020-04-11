0955c17316f3a6dff1d7fd6ad5759ef44a520214
Mitchbux <55109545+Mitchbux@users.noreply.github.com>
2020-04-11 13:50:03 +0200

    Using github.io

--- app/src/main/java/mitchbux/github/io/MainActivity.java
+++ app/src/main/java/mitchbux/github/io/MainActivity.java
@@ -14,8 +14,7 @@ public class MainActivity extends Activity
 		setContentView(R.layout.main);
 		
 		WebView myWebView = (WebView) findViewById(R.id.webview);
-		myWebView.loadUrl("https://mitchbux.github.io/");
+		//myWebView.loadUrl("https://screenshot-kit-r50xkn0t9.now.sh/screenshot?url=http://mitchbux.github.io&width=372&height=904");
 		
-		
-    }
+		myWebView.loadUrl("https://mitchbux.github.io/screenshot.html");}
 }
